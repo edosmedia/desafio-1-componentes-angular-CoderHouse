ce43a0d90a316787c14c59b7440c39452971e590
Eduardo Medrano <eduardomedranop@gmail.com>
2022-11-22 22:42:35 -0300

    desafio 1 - componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { SidenavComponent } from './partials/sidenav/sidenav.component';
+import { ContentComponent } from './partials/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
